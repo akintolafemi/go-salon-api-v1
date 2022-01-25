efcce2d75b7a22d573ccf8f6b8fcfad71e7f02b4
akintolafemi <michealakintola106@yahoo.com>
2022-01-25 10:24:03 -0100

    commented out decorators/*

--- src/decorators/account.types.decorator.ts
+++ src/decorators/account.types.decorator.ts
@@ -10,19 +10,19 @@ export default class AccountTypeGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const accounttypeids = this.reflector.get<number[]>('accounttypeids', context.getHandler());
-    if (!accounttypeids) {
-      return true;
-    }
-    const request = context.switchToHttp().getRequest();
-    const accounttypeid = request.user.accounttypeid;
+    // const accounttypeids = this.reflector.get<number[]>('accounttypeids', context.getHandler());
+    // if (!accounttypeids) {
+    //   return true;
+    // }
+    // const request = context.switchToHttp().getRequest();
+    // const accounttypeid = request.user.accounttypeid;
 
-    if (!accounttypeids.includes(Number(accounttypeid))) {
-      throw new HttpException(
-        ResponseManager.standardResponse("fail", HttpStatus.FORBIDDEN, `you are not allowed to access the resource`, null),
-        HttpStatus.FORBIDDEN,
-      );
-    }
+    // if (!accounttypeids.includes(Number(accounttypeid))) {
+    //   throw new HttpException(
+    //     ResponseManager.standardResponse("fail", HttpStatus.FORBIDDEN, `you are not allowed to access the resource`, null),
+    //     HttpStatus.FORBIDDEN,
+    //   );
+    // }
 
     return true;
   }
